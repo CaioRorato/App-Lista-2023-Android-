ea6eed1ae3d99bb8f19c19f49d6acffee9fa90fe
Caio V. Rorato <caiororato2017@gmail.com>
2023-04-12 11:42:22 -0300

    Commit inicial do projeto

---
+++ app/src/main/java/devandroid/caio/app_lista_android_novo/MainActivity.java
@@ -0,0 +1,14 @@
+package devandroid.caio.app_lista_android_novo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
